f0db7d1bf6c5dadfeacb9afb5f4f6a2ce76069e2
boonzy2 <149987326+boonzy2@users.noreply.github.com>
2024-05-30 15:43:18 +0800

    finished paymentOptions user interface

--- js/paymentOptions.js
+++ js/paymentOptions.js
@@ -1,5 +1,14 @@
-function toggleSelection() {
-    var box = document.getElementById("box");
-    box.classList.toggle("selected");
-  }
-  
+document.addEventListener("DOMContentLoaded", function() {
+    var boxContainer = document.getElementById("box-container");
+
+    // Hide the box container initially
+    boxContainer.style.display = "none";
+
+    var payDiv = document.getElementById("pay");
+
+    payDiv.addEventListener("click", function() {
+        // Show the box container when pay is clicked
+        boxContainer.style.display = "block";
+    });
+});
+
