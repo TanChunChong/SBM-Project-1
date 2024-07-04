be0defd3d4db897741e149150bb48a9d480bec74
prestonkhoo <prestonkhooyiteng@gmail.com>
2024-07-04 18:50:57 +0800

    Changed subjectSettings and studyMaterials

---
+++ js/studyMaterials.js
@@ -0,0 +1,17 @@
+const inProgressButton = document.getElementById('inProgressButton');
+
+inProgressButton.addEventListener('click', () => {
+    window.location.href = 'studyMaterials.html';
+});
+
+const leaderboardButton = document.getElementById('leaderboardButton');
+
+leaderboardButton.addEventListener('click', () => {
+    window.location.href = 'leaderboard1.html';
+});
+
+const personaliseButton = document.getElementById('personaliseButton');
+
+personaliseButton.addEventListener('click', () => {
+    window.location.href = 'subjectSettings.html';
+});
